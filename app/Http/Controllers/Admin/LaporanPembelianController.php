aa2ae2c3afcb33b3efc175b94c249be7a7036dcd
RidhoIR <ridhoikhsandria348@gmail.com>
2025-04-10 13:10:40 +0700

    fix error

--- app/Http/Controllers/Admin/LaporanPembelianController.php
+++ app/Http/Controllers/Admin/LaporanPembelianController.php
@@ -1,10 +1,11 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\Models\LaporanPembelian;
 use Illuminate\Http\Request;
 use Inertia\Inertia;
+use App\Http\Controllers\Controller;
 
 class LaporanPembelianController extends Controller
 {
