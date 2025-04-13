32d5196a6047e867f332125f326fdc1ca4b772fd
RidhoIR <ridhoikhsandria348@gmail.com>
2025-04-13 14:42:35 +0700

    update route

--- app/Http/Middleware/CheckRole.php
+++ app/Http/Middleware/CheckRole.php
@@ -2,16 +2,19 @@
 
 // app/Http/Middleware/CheckRole.php
 
+// app/Http/Middleware/RoleMiddleware.php
+
 namespace App\Http\Middleware;
 
 use Closure;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 
 class CheckRole
 {
     public function handle(Request $request, Closure $next, $role)
     {
-        if (!auth()->user()->roles->contains('name', $role)) {
+        if (Auth::check() && Auth::user()->role !== $role) {
             abort(403, 'Unauthorized');
         }
 
--- routes/web.php
+++ routes/web.php
@@ -18,61 +18,56 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-return Inertia::render('Welcome', [
-'canLogin' => Route::has('login'),
-'canRegister' => Route::has('register'),
-'laravelVersion' => Application::VERSION,
-'phpVersion' => PHP_VERSION,
-]);
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
 });
 
 Route::get('/dashboard', function () {
-return Inertia::render('Admin/Dashboard/Dashboard');
+    return Inertia::render('Admin/Dashboard/Dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 Route::middleware('auth')->group(function () {
 
-Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-Route::resource('/todos', TodoController::class)->names('admin.todo');
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+    Route::resource('/todos', TodoController::class)->names('admin.todo');
 
-// Route::resource('/admin/produk', ProductController::class)->names('admin.produk');
-Route::get('/admin/produk', [ProductController::class, 'index'])->name('admin.produk.index');
-Route::put('/admin/produk/{produk}', [ProductController::class, 'update'])->name('admin.produk.update');
-Route::delete('/admin/produk/{produk}', [ProductController::class, 'destroy'])->name('admin.produk.destroy');
-Route::post('/admin/produk', [ProductController::class, 'store'])->name('admin.produk.store');
+    // Route::resource('/admin/produk', ProductController::class)->names('admin.produk');
+    Route::get('/admin/produk', [ProductController::class, 'index'])->name('admin.produk.index');
+    Route::put('/admin/produk/{produk}', [ProductController::class, 'update'])->name('admin.produk.update');
+    Route::delete('/admin/produk/{produk}', [ProductController::class, 'destroy'])->name('admin.produk.destroy');
+    Route::post('/admin/produk', [ProductController::class, 'store'])->name('admin.produk.store');
 
-Route::resource('/admin/dashboard', DashboardController::class)->names('admin.dashboard');
-Route::resource('/admin/rekap', RekapController::class)->names('admin.rekap');
-Route::resource('/admin/Pelanggan', PelangganController::class)->names('admin.pelanggan');
+    Route::resource('/admin/dashboard', DashboardController::class)->names('admin.dashboard');
+    Route::resource('/admin/rekap', RekapController::class)->names('admin.rekap');
+    Route::resource('/admin/Pelanggan', PelangganController::class)->names('admin.pelanggan');
 
-Route::resource('/admin/supplier', SupplierController::class)->names('admin.supplier');
+    Route::resource('/admin/supplier', SupplierController::class)->names('admin.supplier');
 
-Route::get('/admin/supplier', [SupplierController::class, 'index'])->name('admin.supplier.index');
-Route::put('/admin/supplier/{id}', [SupplierController::class, 'update'])->name('admin.supplier.update');
-Route::delete('/admin/supplier/{id}', [SupplierController::class, 'destroy'])->name('admin.supplier.destroy');
-Route::post('/admin/supplier', [ProductController::class, 'store'])->name('admin.supplier.store');
+    Route::get('/admin/supplier', [SupplierController::class, 'index'])->name('admin.supplier.index');
+    Route::put('/admin/supplier/{id}', [SupplierController::class, 'update'])->name('admin.supplier.update');
+    Route::delete('/admin/supplier/{id}', [SupplierController::class, 'destroy'])->name('admin.supplier.destroy');
+    Route::post('/admin/supplier', [ProductController::class, 'store'])->name('admin.supplier.store');
 
 
 
-Route::get('/admin/laporanpembelian/export', [LaporanPembelianController::class,
-'export'])->name('admin.laporanpembelian.export');
+    Route::get('/admin/laporanpembelian/export', [LaporanPembelianController::class,'export'])->name('admin.laporanpembelian.export');
 
-Route::get('/admin/laporanpembelian', [LaporanPembelianController::class,
-'index'])->name('admin.laporanpembelian.index');
-Route::put('/admin/laporanpembelian/{id}', [LaporanPembelianController::class,
-'update'])->name('admin.laporanpembelian.update');
-Route::delete('/admin/laporanpembelian/{id}', [LaporanPembelianController::class,
-'destroy'])->name('admin.laporanpembelian.destroy');
-Route::post('/admin/laporanpembelian', [LaporanPembelianController::class,
-'store'])->name('admin.laporanpembelian.store');
+    Route::get('/admin/laporanpembelian', [LaporanPembelianController::class,'index'])->name('admin.laporanpembelian.index');
+    Route::put('/admin/laporanpembelian/{id}', [LaporanPembelianController::class,'update'])->name('admin.laporanpembelian.update');
+    Route::delete('/admin/laporanpembelian/{id}', [LaporanPembelianController::class,'destroy'])->name('admin.laporanpembelian.destroy');
+    Route::post('/admin/laporanpembelian', [LaporanPembelianController::class,'store'])->name('admin.laporanpembelian.store');
 
-Route::get('/admin/laporanpenjualan', [LaporanPenjualanController::class,'index'])->name('admin.laporanpenjualan.index');
-// Route::put(/admin/laporanpenjualan/{id}', [LaporanPenjualanController::class,'update'])->name('admin.laporanpenjualan.update');
-// Route::delete('/admin/laporanpenjualan/{id}', [LaporanPenjualanController::class,'destroy'])->name('admin.laporanpenjualan.destroy');
-// Route::post('/admin/laporanpenjualan', [LaporanPenjualanController::class,'store'])->name('admin.laporanpenjualan.store');
+    Route::get('/admin/laporanpenjualan', [LaporanPenjualanController::class, 'index'])->name('admin.laporanpenjualan.index');
+    // Route::put(/admin/laporanpenjualan/{id}', [LaporanPenjualanController::class,'update'])->name('admin.laporanpenjualan.update');
+    // Route::delete('/admin/laporanpenjualan/{id}', [LaporanPenjualanController::class,'destroy'])->name('admin.laporanpenjualan.destroy');
+    // Route::post('/admin/laporanpenjualan', [LaporanPenjualanController::class,'store'])->name('admin.laporanpenjualan.store');
 
 });
 
-require __DIR__.'/auth.php';
+require __DIR__ . '/auth.php';
