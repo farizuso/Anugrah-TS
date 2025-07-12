a2f28531361e709e9138bc61c7e0a89018191dff
RidhoIR <ridhoikhsandria348@gmail.com>
2025-07-12 22:49:12 +0700

    fix tgl

--- resources/views/pdf/invoice.blade.php
+++ resources/views/pdf/invoice.blade.php
@@ -156,8 +156,8 @@
             <td>
                 @if($item->tipe_item === 'sewa')
                     <small>
-                        Sewa: Rp {{ number_format(100000, 0, ',', '.') }} x {{ $item->Arr::query($array) }}<br>
-                        Gas: Rp {{ number_format($hargaGas, 0, ',', '.') }} x {{ $item->Arr::query($array) }}
+                        Sewa: Rp {{ number_format(100000, 0, ',', '.') }} x {{ $item->quantity }}<br>
+                        Gas: Rp {{ number_format($hargaGas, 0, ',', '.') }} x {{ $item->quantity }}
                     </small>
                 @else
                     Rp {{ number_format($item->harga, 0, ',', '.') }}
